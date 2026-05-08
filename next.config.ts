196212cf00f7ca63b7d7fa67651450e943ee0223
Manuel Quintana <73404112+copequintana@users.noreply.github.com>
2026-05-08 16:08:15 -0700

    Configure Next.js output, basePath, and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/hymnext",
+  assetPrefix: "/hymnext/",
 };
 
 export default nextConfig;
